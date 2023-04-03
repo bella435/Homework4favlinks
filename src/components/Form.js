288ab64a5cd55aa1f69141315701c842e2e1f5ae
bella435 <mamadou.diallo19@lc.cuny.edu>
2023-04-02 23:37:58 -0400

    first commit sorry forgot before

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,16 @@
 import { useState } from 'react'
 
-const Form = () => {
+const Form = (props) => {
+  const [linkName, setLinkName] = useState('')
+  const [linkURL, setLinkURL] = useState('')
+
   const handleChange = (event) => {
+    if (event.target.name === 'name') {
+      setLinkName(event.target.value)
+    } else if (event.target.name === 'URL') {
+      setLinkURL(event.target.value)
+    }
+
     /*
             TODO - Logic for changing state based on form changes
         */
@@ -10,14 +19,29 @@ const Form = () => {
   const onFormSubmit = (event) => {
     // to prevent page reload on form submit
     event.preventDefault()
+    props.handleSubmit({
+      name: linkName,
+      URL: linkURL,
+    })
 
+    setLinkName('')
+    setLinkURL('')
     /*
             TODO - Logic for calling props to handle submission and setting state changes
         */
   }
 
   return (
-    <form>
+    <form onSubmit={onFormSubmit}>
+       <label>
+        Name:
+        <input type="text" name="name" value={linkName} onChange={handleChange} />
+      </label>
+      <label>
+        URL:
+        <input type="text" name="URL" value={linkURL} onChange={handleChange} />
+      </label>
+      <button type="submit">Add</button>
       {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
     </form>
   )
--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,15 +1,35 @@
 import { useState } from 'react'
-// import Table from './Table';
-// import Form from './Form';
+
+import Table from './Table';
+ import Form from './Form';
 
 const LinkContainer = (props) => {
-  const handleRemove = (index) => {
+  const [favLinks, setFavLinks] = useState([
+    {
+      name: 'Google',
+      URL: 'https://www.google.com',
+    },
+    {
+      name: 'Facebook',
+      URL: 'https://www.facebook.com',
+    },
+    {
+      name: 'Twitter',
+      URL: 'https://www.twitter.com',
+    },
+  ])
+  
+  const removeLink = (index) => {
+    const newFavLinks = [...favLinks]
+    newFavLinks.splice(index, 1)
+    setFavLinks(newFavLinks)
     /*
             TODO - Create logic for setting the state to filter array and remove favLink at index
         */
   }
 
   const handleSubmit = (favLink) => {
+    setFavLinks([...favLinks, favLink])
     /*
             TODO - Create logic to set state and add new favLink to favLinks array in state
         */
@@ -19,12 +39,15 @@ const LinkContainer = (props) => {
     <div className="container">
       <h1>My Favorite Links</h1>
       <p>Add a new url with a name and link to the table.</p>
+      
       {/*TODO - Add Table Component */}
-
+      <Table linkData={favLinks} removeLink={removeLink} />
       <br />
 
       <h3>Add New</h3>
       {/*TODO - Add Form Component */}
+      <Form handleSubmit={handleSubmit} />
+       
     </div>
   )
 }
